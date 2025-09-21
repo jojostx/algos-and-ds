ef5cbac40221e0c9b67cbc023dd1b863aff2aa36
jojostx <ikuskid7@gmail.com>
2025-09-22 00:53:08 +0100

    balanced brackets problem

---
+++ data_structures/week1_basic_data_structures/1_brackets_in_code/check_brackets.py
@@ -0,0 +1,37 @@
+# python3
+
+from collections import namedtuple
+
+Bracket = namedtuple("Bracket", ["char", "position"])
+
+
+def are_matching(left, right):
+    return (left + right) in ["()", "[]", "{}"]
+
+
+def find_mismatch(text):
+    opening_brackets_stack = []
+    for i, next in enumerate(text):
+        if next in "([{":
+            opening_brackets_stack.append(Bracket(next, i))
+
+        if next in ")]}":
+            if not opening_brackets_stack:
+                return i
+            else:
+                opener = opening_brackets_stack.pop()
+                if not are_matching(opener.char, next): 
+                    return i
+    if opening_brackets_stack:
+        return opening_brackets_stack[0].position
+    
+    return "Success"
+
+def main():
+    text = input()
+    mismatch = find_mismatch(text)
+    print(mismatch)
+
+
+if __name__ == "__main__":
+    main()
